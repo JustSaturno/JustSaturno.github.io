08ff8027d590696e2ba696167b7cc5a7ea81e0f5
igorhenriqueg <igorhgoncalvess@gmail.com>
2023-08-02 11:35:03 -0300

    Att

--- app.js
+++ app.js
@@ -12,3 +12,15 @@ const observer  = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+const observer2  = new IntersectionObserver((entries2) => {
+    entries2.forEach((entry2) => {
+        console.log(entry2)
+        if (entry2.isIntersecting) {
+            entry2.target.classList.add('show2')
+        }
+    });
+});
+
+const hiddenElements2 = document.querySelectorAll('.hidden2');
+hiddenElements2.forEach((ele) => observer2.observe(ele));
